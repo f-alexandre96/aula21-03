9ea6296bd58a6736839f5abccc4185336da4cd78
Fernando Alexandre da Silva <fernando17034@gmail.com>
2025-03-21 20:31:00 -0300

    liga e desliga lampada

---
+++ script.js
@@ -0,0 +1,10 @@
+function ligar(){
+   let lampada = document.getElementById('lampada'); //tag id = lampada
+   lampada.src = "imagens/acesa.jpg";
+
+}
+
+function desligar(){
+    document.getElementById('lampada').src = "imagens/apagada.png";
+
+}
